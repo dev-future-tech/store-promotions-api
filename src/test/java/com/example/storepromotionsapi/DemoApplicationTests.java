31cd58f4286b327166d074b2e67497c33b5edb8b
Anthony Ikeda <anthony.ikeda@gmail.com>
2022-06-15 23:00:02 +0000

    Initial setup of the application.

---
+++ src/test/java/com/example/storepromotionsapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storepromotionsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
